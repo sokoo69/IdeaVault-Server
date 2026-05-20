890f02fabb3d5e757894a32a2d649b3479592e6d
saykot biswas shawon <shawon.saykot2023@gmail.com>
2026-05-20 23:09:19 +0600

    Added global constants and HTTP status configurations

---
+++ utils/constants.js
@@ -0,0 +1,16 @@
+module.exports = {
+  DB_COLLECTIONS: {
+    USERS: 'user',
+    IDEAS: 'ideas',
+    COMMENTS: 'comments'
+  },
+  HTTP_STATUS: {
+    OK: 200,
+    CREATED: 201,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    SERVER_ERROR: 500
+  }
+};
